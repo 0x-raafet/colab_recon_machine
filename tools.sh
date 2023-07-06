990ea976a80abebf3693793392c1d74c9eb7e93d
cobra shell <109506437+0x-raafet@users.noreply.github.com>
2023-07-06 15:38:38 +0100

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -30,6 +30,8 @@ install_subdomain_tools() {
     echo -e "[SUBDOMAINS ENUMERATION] Golang installation in progress ..."
     wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
     rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+    echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+    source $HOME/.profile
     
     # Install Recon-ng
     echo -e "[SUBDOMAINS ENUMERATION] Recon-ng installation in progress ..."
